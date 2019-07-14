c0ca5339c647d464861ee65171586bbd722e6789
kartonki <kartoncodes@outlook.com>
2019-07-13 19:01:00 -0700

    Uploading tokenized configuration file

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const API_URL = '__APIURL__';
+const API_KEY = '__APIKEY__';
+const AI_KEY  = '__AIKEY__';
+
+
+
+// 019e8f375549e0bbd4a4191862ebc88f
+
+// Images
+// An image URL looks like this example:
+// http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
+
+const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+//Sizes: w300, w780, w1280, original
+const BACKDROP_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+const POSTER_SIZE = 'w500'
+
+export {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE
+}
